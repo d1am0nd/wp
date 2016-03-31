6162c0461f1c601e51d17e1dcd5acff6790a17ca
Dev Kordes <devkordes@yahoo.com>
2016-03-31 23:39:22 +0200

    Check if img url starts with http*

--- app/Console/Commands/UpdatePageThumbnail.php
+++ app/Console/Commands/UpdatePageThumbnail.php
@@ -55,11 +55,17 @@ class UpdatePageThumbnail extends Command
                 $property = $meta->getAttribute('property');
                 if($property == "og:image"){
                     $thumbnailUrl = $meta->getAttribute('content');
+                    if(!$this->startsWith($thumbnailUrl, 'http://') || $this->startsWith($thumbnailUrl, 'https://'))
+                        $thumbnailUrl = 'http://' . $thumbnailUrl;
                     $fileName = '/thumbnails/pages/' . $id . '.jpg';
                     $absolutePath = public_path() . $fileName;
-                    // Save thumbnail to public/thumbnails/pages
-                    $img = \Image::make($thumbnailUrl)->fit(480, 280);
-                    $img->save($absolutePath);
+                    try{
+                        // Save thumbnail to public/thumbnails/pages
+                        $img = \Image::make($thumbnailUrl)->fit(480, 280);
+                        $img->save($absolutePath);
+                    }catch(Exception $e){
+                        break;
+                    }
 
                     Page::where('id', $id)->update(['thumbnail_path' => $fileName]);
                     break;
@@ -69,5 +75,10 @@ class UpdatePageThumbnail extends Command
         catch(\Exception $e){
             $this->info($e->getMessage());
         }
+    }
+
+    private function startsWith($haystack, $needle){
+        // search backwards starting from haystack length characters from the end
+        return $needle === "" || strrpos($haystack, $needle, -strlen($haystack)) !== false;
     }
 }
