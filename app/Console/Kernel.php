acb1b1572729fed7789e2208b6db6e29a6abde05
Dev Kordes <devkordes@yahoo.com>
2016-04-02 01:34:56 +0200

    Twitter commands added to scheduler

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -28,7 +28,9 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('inspire')
-                 ->hourly();
+        $schedule->command('twitter:updateRedditCustomWeekly')
+                 ->everyThirtyMinutes();
+        $schedule->command('snapshot:updateUrl')
+                 ->everyThirtyMinutes();
     }
 }
