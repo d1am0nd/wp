521a03664573661d74ea62fcafa9b02556153134
Dev Kordes <devkordes@yahoo.com>
2016-04-29 00:12:40 +0200

    VideoRepository

--- app/Repositories/VideoRepository.php
+++ app/Repositories/VideoRepository.php
@@ -2,7 +2,45 @@
 
 namespace App\Repositories;
 
-class VideoRepository implements VideoRepositoryInterface{
+use DB;
+use Auth;
+use App\Video;
 
+class VideoRepository implements VideoRepositoryInterface {
+
+    public function getVideosWithInfo($forPage = 1, $tag = null, $orderBy = null)
+    {
+        $query = Video::filterOrderBy($orderBy)
+        ->withMyVote()
+        ->whereHasTag($tag);
+        return $this->selectImportant($query, $forPage);
+    }
+
+    public function getVideosWithInfoByTitle($title, $forPage = 1, $tag = null, $orderBy = null)
+    {
+        $query = Video::filterOrderBy($orderBy)
+        ->withMyVote()
+        ->whereHasTag($tag)
+        ->where('title', 'LIKE', '%' . $title . '%');
+        return $this->selectImportant($query, $forPage);
+    }
+
+    private function selectImportant(&$query, $forPage)
+    {
+        return $query->forPage($forPage, 15)
+        ->select([
+            'videos.id',
+            'title',
+            'description',
+            'thumbnail_path',
+            'vote_sum',
+            'videos.created_at',
+            'comment_count',
+            'slug',
+            'url',
+            Auth::check() ? 'my_vote.vote as my_vote' : DB::raw('COALESCE(0) as my_vote'),
+        ])
+        ->get();
+    }
 
 }
--- app/Repositories/VideoRepositoryInterface.php
+++ app/Repositories/VideoRepositoryInterface.php
@@ -4,4 +4,8 @@ namespace App\Repositories;
 
 interface VideoRepositoryInterface {
 
+    public function getVideosWithInfo($forPage = 1, $tag = null, $orderBy = null);
+
+    public function getVideosWithInfoByTitle($forPage = 1, $title, $tag = null, $orderBy = null);
+
 }
