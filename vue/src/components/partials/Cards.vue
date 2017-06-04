693ddaca83142f61aa6a7df2cb0ed4312816cccb
devk <devkordesg@gmail.com>
2017-06-04 03:37:40 +0200

    playing with design

--- vue/src/components/partials/Cards.vue
+++ vue/src/components/partials/Cards.vue
@@ -1,7 +1,29 @@
 <template>
   <div class="cards">
     <input type="text" v-model="filter" placeholder="Filter by name">
-    <div v-for="card in filteredCards">{{ card.name }}</div>
+    <div
+      class="row"
+      v-for="chunk in chunks(filteredCards, 3)">
+      <div
+        class="four columns"
+        v-for="card in chunk">
+        <strong>{{ card.name }}</strong>
+        <ul class="card-list">
+            <li class="atts">
+              <button class="button button-sm">{{ card.type.toLowerCase() }}</button>
+            </li>
+            <li class="atts">
+              <button class="button button-sm">{{ card.rarity.toLowerCase() }}</button>
+            </li>
+            <li class="atts">
+              <button class="button button-sm">{{ card.set.toLowerCase() }}</button>
+            </li>
+            <li class="atts">
+              <button class="button button-sm">{{ card.class.toLowerCase() }}</button>
+            </li>
+        </ul>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -20,6 +42,15 @@ export default {
       return this.cards.cards.filter((val, key) => {
         return vm.cards.attributes.canCardBePlayed(val) && val.name.toLowerCase().indexOf(vm.filter) !== -1
       })
+    }
+  },
+  methods: {
+    chunks (array, chunk) {
+      var tmp = []
+      for (var i = 0, j = array.length; i < j; i += chunk) {
+        tmp.push(array.slice(i, i + chunk))
+      }
+      return tmp
     }
   }
 }
