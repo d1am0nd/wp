ff9be9b30698ac001812c6e3835100e0414aa46c
devk <devkordesg@gmail.com>
2017-06-05 23:31:38 +0200

    backend cache for cards with info

--- app/Providers/BackendServiceProvider.php
+++ app/Providers/BackendServiceProvider.php
@@ -26,7 +26,7 @@ class BackendServiceProvider extends ServiceProvider
         $this->app->bind('App\Repositories\TagRepositoryInterface', 'App\Repositories\TagRepository');
         $this->app->bind('App\Repositories\UserRepositoryInterface', 'App\Repositories\UserRepository');
         $this->app->bind('App\Repositories\PageRepositoryInterface', 'App\Repositories\PageRepository');
-        $this->app->bind('App\Repositories\CardRepositoryInterface', 'App\Repositories\CardRepository');
+        $this->app->bind('App\Repositories\CardRepositoryInterface', 'App\Repositories\Decorators\CardRepository');
         $this->app->bind('App\Repositories\VideoRepositoryInterface', 'App\Repositories\VideoRepository');
         $this->app->bind('App\Repositories\CommentRepositoryInterface', 'App\Repositories\CommentRepository');
         $this->app->bind('App\Repositories\PageTypeRepositoryInterface', 'App\Repositories\PageTypeRepository');
--- app/Repositories/Decorators/CardRepository.php
+++ app/Repositories/Decorators/CardRepository.php
@@ -1,26 +1,19 @@
 <?php
 
-namespace App\Repositories;
+namespace App\Repositories\Decorators;
 
 use Cache;
 use App\Repositories\CardRepository as CardRepositoryOriginal;
 
 class CardRepository extends CardRepositoryOriginal
 {
-    public function __construct(Cache $cache)
-    {
-        parent::__construct();
-
-        $this->cache = $cache;
-    }
-
     public function getCardsWithInfo()
     {
-        if ($this->cache->has('cards_with_info')) {
-            return $this->cache->get('cards_with_info');
+        if (Cache::has('cards_with_info')) {
+            return Cache::get('cards_with_info');
         }
-        $result = $this->parent->getCardsWithInfo();
-        $this->cache->put('cards_with_info', $result, 300);
+        $result = parent::getCardsWithInfo();
+        Cache::put('cards_with_info', $result, 300);
         return $result;
     }
 }
